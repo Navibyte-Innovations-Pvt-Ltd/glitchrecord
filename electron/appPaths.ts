7764253398ddb45f6a8f5fa70c5aa098d54f3d04
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2026-06-09 10:58:17 +0530

    fix(electron/appPaths.ts): update index watching logic for VS Code staging triggers auto-commit

--- electron/appPaths.ts
+++ electron/appPaths.ts
@@ -2,7 +2,11 @@ import path from "node:path";
 import { app } from "electron";
 
 if (process.env["VITE_DEV_SERVER_URL"]) {
+<<<<<<< HEAD
 	const devUserDataPath = path.join(app.getPath("appData"), "GlitchRecord-dev");
+=======
+	const devUserDataPath = path.join(app.getPath("appData"), "GlitchGrab-dev");
+>>>>>>> 6fc7bbcbdb19e82c384b1fc0ff8de872093c645c
 	app.setPath("userData", devUserDataPath);
 	app.setPath("sessionData", path.join(devUserDataPath, "session"));
 }
