595ce1eaded0a3f327fa04d9b817fb8442db03fb
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2026-06-09 10:54:17 +0530

    fix(glitchrecord): add support for ARM architecture

---
+++ glitchrecord.rb
@@ -0,0 +1,25 @@
+cask "glitchrecord" do
+  arch arm: "arm64", intel: "x64"
+
+  version "1.1.14"
+  sha256 arm:   "e669ab7c8bdd4596211937183ee2374545da5482702cab0dfe477c1466422b0f",
+         intel: "85f5183219de0b656400625797ff9299893bd8fbda8455b0f745878b2c729526"
+
+  url "https://github.com/webadderallorg/GlitchRecord/releases/download/v#{version}/GlitchRecord-#{arch}.dmg"
+  name "GlitchRecord"
+  desc "Creator-focused screen recorder with auto-zoom, cursor effects, and more"
+  homepage "https://github.com/webadderallorg/GlitchRecord"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "GlitchRecord.app"
+
+  zap trash: [
+    "~/Library/Application Support/GlitchRecord",
+    "~/Library/Preferences/dev.glitchrecord.app.plist",
+    "~/Library/Saved Application State/dev.glitchrecord.app.savedState",
+  ]
+end
